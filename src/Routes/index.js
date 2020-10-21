de2b3b3b0e461efc2244cb0a2e388bce0c40286e
Iván Pérez <69504134+Sneck44@users.noreply.github.com>
2020-10-20 23:28:54 -0500

    base init

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,5 +1,8 @@
 import Header from "../templates/Header";
 import Home from "../pages/Home";
+import Error404 from "../pages/Error404"
+import getHash from "../utils/getHash";
+import resolveRoutes from "../utils/resolveRoutes";
 
 const routes = {
   "/": Home,
@@ -11,6 +14,10 @@ const router = async () => {
   const header = null || document.getElementById("header");
 
   header.innerHTML = await Header();
+  let hash= getHash ();
+  let route= await resolveRoutes (hash);
+  let render= routes [route]? routes [route] : Error404;
+  main.innerHTML= await render();
 };
 
 export default router;
