9b1ca1ee1914c860ec2aa5f15a747641e39135f6
Iván Pérez <69504134+Sneck44@users.noreply.github.com>
2020-10-23 01:28:24 -0500

    Header in process

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -14,6 +14,8 @@ const router = async () => {
   const header = null || document.getElementById("header");
 
   header.innerHTML = await Header();
+
+
   let hash= getHash ();
   let route= await resolveRoutes (hash);
   let render= routes [route]? routes [route] : Error404;
